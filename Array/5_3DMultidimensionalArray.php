adc2daa56870dc8a14a946bbe60d225d64caac16
taslim-at-538782569624 <taslimarif600@gmail.com>
2020-10-26 21:25:48 +0530

    Php Tutorial

---
+++ Array/5_3DMultidimensionalArray.php
@@ -0,0 +1,13 @@
+<?php
+$arr=array(
+    array(
+        array(1,2,3,4,5),
+        array(6,7,8,9,10)
+    ),array(
+        array(10,11,12,13,14),
+        array(1,2,3,4,5)
+    )
+    );
+
+    echo var_dump($arr);
+?>
